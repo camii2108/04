803d6e6373ffaf6eed6fc7d1751a678cb7b1f357
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-06-22 22:41:05 -0300

    agregando las cards de peliculas favoritas en vista favorites.html

--- frontend/js/favoritas.js
+++ frontend/js/favoritas.js
@@ -1,108 +1,32 @@
 window.onload = () => {
-  const qs = (selector) => document.querySelector(selector);
+  const favoritesContainer = document.getElementById("favoritesContainer");
 
-  let $title = qs("#title");
-  let $rating = qs("#rating");
-  let $awards = qs("#awards");
-  let $release_date = qs("#release_date");
-  let $length = qs("#length");
-  let $movieId = qs("#movieId");
-  let $editar = qs("#editar");
-  let $crear = qs("#crear");
-  let $eliminar = qs("#eliminar");
+  // Obtener las películas favoritas del almacenamiento local
+  const favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies"));
 
-  let id = prompt("Para editar una película, ingrese el ID. De lo contrario, cancele y cree una nueva película.");
+  if (favoriteMovies === null || favoriteMovies.length === 0) {
+    // Si no hay películas favoritas, mostrar un mensaje indicando que no hay nada
+    favoritesContainer.innerHTML = "<p>Aún no tienes películas favoritas.</p>";
+  } else {
+    // Si hay películas favoritas, crear elementos HTML para cada una y agregarlos al contenedor
+    favoriteMovies.forEach((movie) => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
 
-  if (id) {
-      fetch(`http://localhost:3031/api/movies/${id}`)
-          .then((response) => response.json())
-          .then((data) => {
-              let movie = data.data;
-              $movieId.value = movie.id;
-              $title.value = movie.title;
-              $rating.value = movie.rating;
-              $awards.value = movie.awards;
-              $release_date.value = movie.release_date.slice(0, 10);
-              $length.value = movie.length;
-          })
-          .catch((error) => {
-              console.log(error);
-              alert("Error al obtener los datos de la película");
-          });
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
+
+      const p = document.createElement("p");
+      p.textContent = `Rating: ${movie.rating}`;
+
+      const duracion = document.createElement("p");
+      duracion.textContent = `Duración: ${movie.length}`;
+
+      card.appendChild(h1);
+      card.appendChild(p);
+      card.appendChild(duracion);
+
+      favoritesContainer.appendChild(card);
+    });
   }
-
-  $editar.addEventListener("click", () => {
-      let updatedData = {
-          title: $title.value,
-          rating: $rating.value,
-          awards: $awards.value,
-          release_date: $release_date.value,
-          length: $length.value,
-      };
-
-      let settings = {
-          method: "PUT",
-          body: JSON.stringify(updatedData),
-          headers: { "Content-Type": "application/json" }
-      };
-
-      let url = `http://localhost:3031/api/movies/update/${$movieId.value}`;
-
-      fetch(url, settings)
-          .then((response) => response.json())
-          .then(() => {
-              alert("Película modificada");
-          })
-          .catch((error) => {
-              console.log(error);
-              alert("Error al modificar la película");
-          });
-  });
-
-  $crear.addEventListener("click", () => {
-      let updatedData = {
-          title: $title.value,
-          rating: $rating.value,
-          awards: $awards.value,
-          release_date: $release_date.value,
-          length: $length.value,
-      };
-
-      let settings = {
-          method: "POST",
-          body: JSON.stringify(updatedData),
-          headers: { "Content-Type": "application/json" }
-      };
-
-      let url = `http://localhost:3031/api/movies/create`;
-
-      fetch(url, settings)
-          .then((response) => response.json())
-          .then(() => {
-              alert("Película creada");
-              if (response.ok) {
-                  window.location.href = "/home.html";
-              }
-          })
-          .catch((error) => {
-              console.log(error);
-          });
-  });
-
-  $eliminar.addEventListener("click", () => {
-
-      let url = `http://localhost:3031/api/movies/delete/${id}`
-
-      let settings = {
-          method: "DELETE",
-          headers: {"Content-Type" : "application/json"}
-      }
-
-      fetch(url, settings)
-          .then((response => console.log(response)))
-          .then(() => alert("Película borrada"))
-          if (response.ok) {
-              window.location.href = "/home.html";
-        }
-  })
-}
+};
